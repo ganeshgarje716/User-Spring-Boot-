c71792e53d397574d4d0f2b25a829e6001fccb2e
Ganesh <ganeshgarje716@gmail.com>
2026-06-01 10:32:37 +0530

    Add Variables and Annotations

--- SpringBoot-User/src/main/java/com/ganesh/entity/User.java
+++ SpringBoot-User/src/main/java/com/ganesh/entity/User.java
@@ -1,5 +1,55 @@
 package com.ganesh.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String password;
+	
+	private String mobileNo;
+	
+	private String address;
+	
+	private Integer age;
+
+	
+	
+	
+	
+	public User(String name, String email, String password, String mobileNo, String address, Integer age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.mobileNo = mobileNo;
+		this.address = address;
+		this.age = age;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
