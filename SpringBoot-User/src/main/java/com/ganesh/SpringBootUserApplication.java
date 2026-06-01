2f5afdb3545d2cdda30c71446384fcd4cfbc49d1
Ganesh <ganeshgarje716@gmail.com>
2026-06-01 10:47:22 +0530

    implements CommandLineRunner and Create UserRepository interface Variable

--- SpringBoot-User/src/main/java/com/ganesh/SpringBootUserApplication.java
+++ SpringBoot-User/src/main/java/com/ganesh/SpringBootUserApplication.java
@@ -1,13 +1,32 @@
 package com.ganesh;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ganesh.entity.User;
+import com.ganesh.repository.UserRepository;
+
 @SpringBootApplication
-public class SpringBootUserApplication {
+public class SpringBootUserApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootUserApplication.class, args);
+	}
+	
+	
+	
+	@Autowired
+	UserRepository UR;
+	
+	
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+		
 	}
 
 }
