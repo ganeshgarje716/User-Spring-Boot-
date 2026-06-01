7770332267ae87a8d8cc6968495b0c7ec5c14820
Ganesh <ganeshgarje716@gmail.com>
2026-06-01 10:53:28 +0530

    Create email and mobileNo is Unique

--- SpringBoot-User/src/main/java/com/ganesh/entity/User.java
+++ SpringBoot-User/src/main/java/com/ganesh/entity/User.java
@@ -1,5 +1,6 @@
 package com.ganesh.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,10 +19,12 @@ public class User {
 	
 	private String name;
 	
+	@Column(unique = true)
 	private String email;
 	
 	private String password;
 	
+	@Column(unique = true)
 	private String mobileNo;
 	
 	private String address;
