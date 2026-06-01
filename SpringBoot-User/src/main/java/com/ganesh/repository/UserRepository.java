958719f1326ff6846edf23a2a78349e71db077f8
Ganesh <ganeshgarje716@gmail.com>
2026-06-01 10:44:35 +0530

    extends JpaRepository Interface

--- SpringBoot-User/src/main/java/com/ganesh/repository/UserRepository.java
+++ SpringBoot-User/src/main/java/com/ganesh/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package com.ganesh.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ganesh.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
